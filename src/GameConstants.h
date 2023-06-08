203b65fc33672e14f9996d6d3c5b2d07be80fd9f
Kristian Kalmar <kalmakri@fit.cvut.cz>
2023-06-08 15:15:38 +0200

    3-master: Edit config style & change names for naming consistency

---
+++ src/GameConstants.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define GAME_WIN_Y 5 //30
+#define GAME_WIN_X 10
+#define TEXT_WIN_Y 5
+
+#define GAME_UPDATE_RATE 64
+
+#define CONF_SCENE_DELIM "#SCENE"
+#define CONF_OBJECT_DELIM "#OBJECT"
+#define CONF_PARAM_DELIM "--"
+#define CONF_COMMENT_DELIM "//"
